92f526119359c7851a967ca6f216ab6e9a1fd418
Lagbjorn <dbegal@mail.ru>
2021-06-06 09:28:07 +0300

    Add _get_filmworks_by_person_job to keep it DRY

--- src/services/person.py
+++ src/services/person.py
@@ -7,6 +7,7 @@ from fastapi import Depends
 
 from db.elastic import get_elastic
 from db.redis import get_redis
+from models.film import Film
 from models.person import Person
 
 CACHE_EXPIRE = 60  # seconds
@@ -39,36 +40,25 @@ class PersonService:
         if not data:
             return None
         data = data["_source"]
-        data['actor_in'] = await self._get_filmworks_by_actor(person_id)
-        data['creator_in'] = await self._get_filmworks_by_creator(person_id)
+        data['actor_in'] = await self._get_actor_filmworks(person_id)
+        data['creator_in'] = await self._get_created_filmworks(person_id)
         return Person.parse_obj(data)
 
     async def _put_to_cache(self, person: Person):
         await self.cache.set(person.id, person.json(), expire=CACHE_EXPIRE)
 
-    async def _get_filmworks_by_actor(self, person_id: str) -> Optional[List[dict]]:
+    async def _get_actor_filmworks(self, person_id: str) -> Optional[List[dict]]:
         """
         Find films where given person played some role
         :param person_id:
         :return: List[dict]: [{"uuid": filmwork_uuid, "role": actor_role}, ...]
         """
-        query = {
-            'query': {
-                'nested': {
-                    'path': 'actors',
-                    'query': {
-                        'match': {
-                            'actors.id': person_id
-                        }
-                    }
-                }
-            }
-        }
-        search_results = await self.storage.search(body=query, index='movies')
-        films = [{"uuid": film['_id'], "role": None} for film in search_results["hits"]["hits"]]
+        films = await self._get_filmworks_by_person_job(person_id, 'actor')
+        # TODO: add role when it will exist in index schema
+        films = [{"uuid": film.id, "role": None} for film in films]
         return films
 
-    async def _get_filmworks_by_creator(self, person_id: str) -> Optional[List[dict]]:
+    async def _get_created_filmworks(self, person_id: str) -> Optional[List[dict]]:
         """
         Find films which given person did create
         :param person_id:
@@ -77,22 +67,29 @@ class PersonService:
         jobs = ['writer', 'director', 'actor']
         all_films = []
         for job in jobs:
-            query = {
-                'query': {
-                    'nested': {
-                        'path': f'{job}s',
-                        'query': {
-                            'match': {
-                                f'{job}s.id': person_id
-                            }
+            films = await self._get_filmworks_by_person_job(person_id, job)
+            films = [{"uuid": film.id, "job": job} for film in films]
+            all_films.extend(films)
+        return all_films
+
+    async def _get_filmworks_by_person_job(self, person_id: str, job: str) -> List[Film]:
+        query = {
+            'query': {
+                'nested': {
+                    'path': f'{job}s',
+                    'query': {
+                        'match': {
+                            f'{job}s.id': person_id
                         }
                     }
                 }
             }
-            search_results = await self.storage.search(body=query, index='movies')
-            films = [{"uuid": film['_id'], "job": job} for film in search_results["hits"]["hits"]]
-            all_films.extend(films)
-        return all_films
+        }
+        search_results = await self.storage.search(body=query, index='movies')
+        # TODO: remove mock when genres field in movies index will be fixed
+        films = [Film.parse_obj({**film["_source"], **{"genres": [], "genres_names": []}})
+                 for film in search_results["hits"]["hits"]]
+        return films
 
 
 @lru_cache()
