2bf56fea0cd3ebc56edbf0eeea7cd84dc0818fdf
Lagbjorn <dbegal@mail.ru>
2021-06-17 21:41:09 +0300

    Create docker-compose environment for tests

---
+++ tests/functional/settings.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSettings, Field
+
+
+class TestSettings(BaseSettings):
+    es_host: str = Field('http://127.0.0.1', env='ELASTIC_HOST')
+    es_port: str = Field('9200', env='ELASTIC_PORT')
+    redis_host: str = Field('http://127.0.0.1', env='REDIS_HOST')
+    redis_port: str = Field('6379', env='REDIS_PORT')
---
+++ tests/functional/utils/wait_for_redis.py
@@ -0,0 +1,23 @@
+import backoff
+import redis
+
+from tests.functional.settings import TestSettings
+
+settings = TestSettings()
+REDIS_WAIT_TIME = 300
+
+
+# TODO: add logs
+@backoff.on_exception(backoff.expo,
+                      redis.exceptions.ConnectionError,
+                      max_time=REDIS_WAIT_TIME)
+def wait_for_redis(r):
+    r.ping()
+
+
+if __name__ == "__main__":
+    host = settings.redis_host
+    port = settings.redis_port
+    r = redis.Redis(host=host, port=port)
+
+    wait_for_redis(r)
