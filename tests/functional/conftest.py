849ff4acad7a2d671fd84be5fd0fbfebec19245c
Lagbjorn <82833865+lagbjorn@users.noreply.github.com>
2021-06-19 00:00:55 +0300

    Done #13: use separate ES container for tests

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,7 +1,10 @@
+import os
 from dataclasses import dataclass
 
 import aiohttp
 import pytest
+from elasticsearch import AsyncElasticsearch
+from elasticsearch.client import SnapshotClient
 from multidict import CIMultiDictProxy
 
 from functional.settings import TestSettings
@@ -23,6 +26,41 @@ async def session():
     session = aiohttp.ClientSession()
     yield session
     await session.close()
+
+
+@pytest.fixture
+async def es_client():
+    host = settings.es_host + ":" + settings.es_port
+    client = AsyncElasticsearch(hosts=host)
+    yield client
+    await client.close()
+
+
+@pytest.fixture(autouse=True)
+async def es_from_snapshot(es_client):
+    """
+    Fill ElasticSearch data from snapshot and cleanup after tests.
+    """
+    snapshots = SnapshotClient(es_client)
+    body = {
+        "type": "fs",
+        "settings": {
+            "location": os.path.join(settings.es_snapshot_loc, settings.es_snapshot_repo)
+        }
+    }
+    await snapshots.create_repository(repository=settings.es_snapshot_repo,
+                                      body=body,
+                                      verify=True)
+    await snapshots.restore(repository=settings.es_snapshot_repo,
+                            snapshot="snapshot_1",
+                            wait_for_completion=True)
+
+    yield
+    # Cleanup
+    indices = await es_client.indices.get_alias()
+    for index in indices.keys():
+        await es_client.indices.delete(index=index)
+    await snapshots.delete_repository(repository=settings.es_snapshot_repo)
 
 
 @pytest.fixture
