391d8ccfd69775e0e08720ae587a5373412f9cf3
Lagbjorn <82833865+lagbjorn@users.noreply.github.com>
2021-06-20 11:18:45 +0300

    Add test for person not found

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -4,4 +4,11 @@ import pytest
 @pytest.mark.asyncio
 async def test_search_person(make_get_request):
     response = await make_get_request("/person/search/", params={"query": "Lucas"})
-    assert response.status in (200, 404)
+    assert response.status == 200
+
+
+@pytest.mark.asyncio
+async def test_person_not_found(make_get_request):
+    some_id = "this-is-not-even-an-uuid"
+    response = await make_get_request(f"/person/{some_id}", params={})
+    assert response.status == 404
