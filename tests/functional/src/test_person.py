e772cbafa5c3bc4b52d8af4e548edc6c35788b60
Lagbjorn <dbegal@mail.ru>
2021-06-21 00:04:17 +0300

    Add test by id

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -12,3 +12,11 @@ async def test_person_not_found(make_get_request):
     some_id = "this-is-not-even-an-uuid"
     response = await make_get_request(f"/person/{some_id}", params={})
     assert response.status == 404
+
+
+@pytest.mark.asyncio
+async def test_person_by_id(make_get_request):
+    some_id = "239f6e94-b317-4f10-bb0c-ef86dfe33d8a"
+    response = await make_get_request(f"/person/{some_id}", params={})
+    print(response.body)
+    assert response.status == 200
