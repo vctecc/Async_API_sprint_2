a63eb282f5f0149c33a4775f2fc63ac8674aa065
Lagbjorn <dbegal@mail.ru>
2021-06-21 18:45:30 +0300

    Temporarily request es_from_snapshot fixture explicitly (until we move to another "from json" loading mechanism)

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -2,20 +2,20 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_search_person(make_get_request):
+async def test_search_person(make_get_request, es_from_snapshot):
     response = await make_get_request("/person/search/", params={"query": "Lucas"})
     assert response.status == 200
 
 
 @pytest.mark.asyncio
-async def test_person_not_found(make_get_request):
+async def test_person_not_found(make_get_request, es_from_snapshot):
     some_id = "this-is-not-even-an-uuid"
     response = await make_get_request(f"/person/{some_id}", params={})
     assert response.status == 404
 
 
 @pytest.mark.asyncio
-async def test_person_by_id(make_get_request):
+async def test_person_by_id(make_get_request, es_from_snapshot):
     some_id = "239f6e94-b317-4f10-bb0c-ef86dfe33d8a"
     response = await make_get_request(f"/person/{some_id}", params={})
     print(response.body)
