1da12ca51d69dbd4452a066ed87cd866921782d9
Lagbjorn <82833865+lagbjorn@users.noreply.github.com>
2021-06-21 21:56:47 +0300

    Add generalized test for 404 routes

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -13,14 +13,6 @@ async def test_search_person(make_get_request, es_from_snapshot, expected_json_r
 
 
 @pytest.mark.asyncio
-async def test_person_not_found(make_get_request, es_from_snapshot):
-    some_id = "this-is-not-even-an-uuid"
-    response = await make_get_request(f"/person/{some_id}")
-    assert response.status == 404
-    assert response.body == {"detail": "person not found"}
-
-
-@pytest.mark.asyncio
 async def test_person_by_id(make_get_request, es_from_snapshot, expected_json_response):
     some_id = "239f6e94-b317-4f10-bb0c-ef86dfe33d8a"
     response = await make_get_request(f"/person/{some_id}")
