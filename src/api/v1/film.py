ae938f6e299f62c375f606955ded01f5993fe188
vctecc <vctecc@gmail.com>
2021-06-24 22:32:18 +0300

    fix: safe work with paging in films API

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -21,8 +21,8 @@ async def film_details(film_id: str, film_service: FilmService = Depends(get_fil
 @router.get("/", response_model=List[FilmPreview])
 async def films_index(
         sort: Optional[str] = None,
-        size: Optional[int] = Query(FILM_PAGE_SIZE, alias="page[size]"),
-        page: Optional[int] = Query(FILM_PAGE_NUMBER, alias="page[number]"),
+        size: Optional[int] = Query(FILM_PAGE_SIZE, alias="page[size]", ge=1),
+        page: Optional[int] = Query(FILM_PAGE_NUMBER, alias="page[number]", ge=1),
         genre: Optional[str] = Query(None, alias="filter[genre]"),
         film_service: FilmService = Depends(get_film_service)
 ) -> List[FilmPreview]:
@@ -33,8 +33,8 @@ async def films_index(
 async def films_index(
         query: Optional[str] = None,
         sort: Optional[str] = None,
-        size: Optional[int] = Query(FILM_PAGE_SIZE, alias="page[size]"),
-        page: Optional[int] = Query(FILM_PAGE_NUMBER, alias="page[number]"),
+        size: Optional[int] = Query(FILM_PAGE_SIZE, alias="page[size]", ge=1),
+        page: Optional[int] = Query(FILM_PAGE_NUMBER, alias="page[number]", ge=1),
         genre: Optional[str] = Query(None, alias="filter[genre]"),
         film_service: FilmService = Depends(get_film_service)
 ) -> List[FilmPreview]:
