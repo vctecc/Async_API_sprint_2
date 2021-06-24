a7a2ba9a6b076e77816a07573077c031c7131a2b
Lagbjorn <82833865+lagbjorn@users.noreply.github.com>
2021-06-24 13:45:11 +0300

    Test `/person/search/` pagination

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -26,3 +26,81 @@ async def test_person_films_by_id(make_get_request, es_from_snapshot, expected_j
     response = await make_get_request(f"/person/{some_id}/film")
     assert response.status == 200
     assert response.body == expected_json_response
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+            'page_number', 'page_size'
+    ),
+    (
+            (1, 1),
+            ("1", "1"),
+            (1, 5),
+            (5, 1),
+            (1, 4164),  # total number of persons
+    )
+)
+async def test_search_person_pagination(make_get_request, es_from_snapshot, page_number, page_size):
+    """Pagination tests for common cases"""
+    # only check response status and number of elements here
+    response = await make_get_request("/person/search/",
+                                      params={
+                                          "page[number]": page_number,
+                                          "page[size]": page_size
+                                      }
+                                      )
+    assert response.status == 200
+    assert len(response.body) == int(page_size)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+            'page_number', 'page_size', 'expected_count'
+    ),
+    (
+            (1, 10000, 4164),  # we don't have that many persons in index
+            (42, 100, 64),
+            (100, 100, 0),
+    )
+)
+async def test_search_person_pagination_borders(make_get_request,
+                                                es_from_snapshot,
+                                                page_number,
+                                                page_size,
+                                                expected_count):
+    response = await make_get_request("/person/search/",
+                                      params={
+                                          "page[number]": page_number,
+                                          "page[size]": page_size
+                                      }
+                                      )
+    assert response.status == 200
+    assert len(response.body) == expected_count
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+            'page_number', 'page_size'
+    ),
+    (
+            (-1, 5),
+            (-1, -1),
+            (1, -1),
+            ("str_number", "str_size"),
+            (1.5, 1.7),
+    )
+)
+async def test_search_person_pagination_invalid_input(make_get_request,
+                                                      es_from_snapshot,
+                                                      page_number,
+                                                      page_size):
+    response = await make_get_request("/person/search/",
+                                      params={
+                                          "page[number]": page_number,
+                                          "page[size]": page_size
+                                      }
+                                      )
+    assert response.status == 422
