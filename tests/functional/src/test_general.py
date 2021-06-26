99cbb3cbbcd42aaf347db52153ec357973cd6e5d
Andrey Kaynara <andrey.kaynara@gmail.com>
2021-06-26 10:23:40 +0700

    Add tests for genres

--- tests/functional/src/test_general.py
+++ tests/functional/src/test_general.py
@@ -9,9 +9,10 @@ import pytest
             ("/person/wrong-id/film", {"detail": "person not found"}),
             ("/film/wrong-id", {"detail": "film not found"}),
             ("/absolutely_wrong_route", {"detail": "Not Found"}),
+            ("/genre/wrong-id", {"detail": "genre not found"})
     )
 )
-async def test_404_routes(make_get_request, es_from_snapshot, route, body):
+async def test_404_routes(make_get_request, initialize_environment, route, body):
     response = await make_get_request(route)
     assert response.status == 404
     assert response.body == body
